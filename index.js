650bb1e8b7213019cf580ad80babffbfa1ceac0c
tonmoyprogrammer <debtonmoy583@gmail.com>
2026-02-17 03:47:43 +0600

    modal done commit

--- index.js
+++ index.js
@@ -6,9 +6,59 @@ const loadProducts= ()=>
     
 }
 
+const loadShowmodal =(id)=>
+{
+    const fetchData = `https://fakestoreapi.com/products/${id}`
+    fetch(fetchData)
+    .then(res => res.json())
+    .then(data => displayShowmodal(data))
+}
+const displayShowmodal = (product)=>
+{
+   const modalContainer = document.getElementById("modal-container");
+   modalContainer.innerHTML="";
+   
+   
+        const modal = document.createElement("div");
+        modal.innerHTML=`
+       
+
+       <div class="p-10 space-y-4">
+        <div class= "" >
+               <p class="font-bold text-3xl">${product.category}</p>
+        </div>
+
+        <p class="font-semibold l text-gray-600">${product.title}</p>
+        <p class="font-semibold l text-gray-600">${product.description}</p>
+
+
+       
+        <div class="flex justify-between " >
+        <p class=" bg-[#BADEFF40] py-2 px-5 font-semibold rounded-2xl text-violet-700" >
+        <i class="fa-solid fa-dollar-sign"></i> ${product.price}</p>
+        <p class="text-gray-600" ><i class="fa-regular fa-star "></i> 
+        <span> (${product.rating.rate})</p>
+        </div>
+        <button class="btn btn-primary px-10 rounded-2xl "><i class="fa-solid fa-cart-arrow-down"></i>
+          Add
+        </button>
+        </div>
+
+       </div>
+        </div>
+        
+        `
+           
+        modalContainer.appendChild(modal);
+          document.getElementById("my_modal_5").showModal();
+  
+
+ 
+}
+
 const displayProducts =(products)=>
 {
-    console.log(products)
+    
     const cartContainer = document.getElementById("cart-container");
         // cartContainer.innerHTML="";
 
@@ -40,7 +90,7 @@ const displayProducts =(products)=>
         <p class="font-2xl font-bold"><i class="fa-solid fa-dollar-sign">${product.price}</i></p>
 
         <div class="flex justify-between gap-2.5 "> 
-        <button class="btn px-10 rounded-2xl text-gray-500"><i class="fa-solid fa-eye"></i>
+        <button onClick="loadShowmodal(${product.id})" class="btn px-10 rounded-2xl text-gray-500"><i class="fa-solid fa-eye"></i>
             Details
         </button>
 
